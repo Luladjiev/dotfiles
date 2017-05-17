1911cfbdf041757f91dfb7e4d9e5828759ad33c8
Peter Luladjiev <luladjiev@gmail.com>
2017-05-17 16:04:49 +0300

    [Doom] Change evil-escape key sequence and javascript indentations

--- emacs-doom/config.el
+++ emacs-doom/config.el
@@ -1,3 +1,15 @@
-;;; private/luladjiev/config.el
+;;; package --- private/luladjiev/config.el
+
+;;; Commentary:
+;;; Private module to extend Doom
+
+;;; Code:
+(when (featurep 'evil)
+  (setq evil-escape-key-sequence "fd"))
+
+(add-hook! 'js2-mode-hook
+  (setq js-switch-indent-offset 2)
+  (setq js2-indent-switch-body t))
 
 (set-face-attribute 'default nil :font "Source Code Pro-14" )
+;;; config.el ends here
