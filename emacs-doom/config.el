213c09a34a26a85a364f969876ae2c112cf78a8e
Peter Luladjiev <luladjiev@gmail.com>
2017-05-21 21:39:46 +0300

    [Doom] Remove obsolete settings

--- emacs-doom/config.el
+++ emacs-doom/config.el
@@ -7,9 +7,6 @@
 (when (featurep 'evil)
   (setq evil-escape-key-sequence "fd"))
 
-(when (eq system-type 'windows-nt)
-  (setq dumb-jump-prefer-searcher 'rg))
-
 (add-hook! 'js2-mode-hook
   (setq js-switch-indent-offset 2)
   (setq js2-indent-switch-body t))
